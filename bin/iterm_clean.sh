08450d63857a7d3f233082bbbfa30f62301a8d62
plongitudes <3813410+plongitudes@users.noreply.github.com>
2018-02-08 20:50:52 -0800

    adding some smudge/clean filters for iterm plist username scrubbing

---
+++ bin/iterm_clean.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+sed -i "" "s/$USER/__USERNAME_PLACEHOLDER__/g" ${HOME}/.dotfiles/iterm2/com.googlecode.iterm2.plist
+
---
+++ bin/iterm_smudge.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+sed -i "" "s/__USERNAME_PLACEHOLDER__/$USER/g" ${HOME}/.dotfiles/iterm2/com.googlecode.iterm2.plist
+
