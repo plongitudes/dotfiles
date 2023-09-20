889dbe823f998e922c6f56b04e47f1a29d3eae62
plongitudes <3813410+plongitudes@users.noreply.github.com>
2023-09-20 10:43:37 -0700

    python-related configs

---
+++ ipython_startup.py
@@ -0,0 +1,12 @@
+# should live in ~/.ipython/profile_default/startup
+
+import logging
+import os
+import json
+import readline
+import sys
+import black
+import IPython
+import pdb
+
+from pprint import pprint as pp
