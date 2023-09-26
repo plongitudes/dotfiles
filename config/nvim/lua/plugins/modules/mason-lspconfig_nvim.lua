4e696ae8d12abd409fac48ffc338e551ee968e14
plongitudes <3813410+plongitudes@users.noreply.github.com>
2023-09-26 16:47:23 -0700

    pyright config fix
    do lua_lsp next!

--- config/nvim/lua/plugins/modules/mason-lspconfig_nvim.lua
+++ config/nvim/lua/plugins/modules/mason-lspconfig_nvim.lua
@@ -2,5 +2,28 @@ return {
   'williamboman/mason-lspconfig.nvim',
   dependencies = {
     'neovim/nvim-lspconfig', -- lsp server configuration
-  }
+  },
+  config = function()
+    require('lspconfig').pyright.setup({
+      cmd = {'pyright-langserver', '--stdio'},
+      filetypes = {'python'},
+      settings = {
+        pyright = {
+          disableLanguageServices = true,
+        },
+        python = {
+          analysis = {
+            autoImportCompletions = true,
+            autoSearchPaths = true,
+            diagnosticMode = 'workspace',
+            useLibraryCodeForTypes = true,
+            extraPaths = {},
+            pylintPath = {},
+          },
+          good_names_rgxs = {'[a-z]{1,3}'},
+        },
+      },
+      single_file_support = true,
+    }) -- end pyright
+  end
 }
