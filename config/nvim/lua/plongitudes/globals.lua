97218b046d0683026e9e96e4d59cf1d577dcf78d
Tony Etienne <github@tonyetienne.com>
2023-09-27 14:58:45 -0700

    fixing table print function to print even if the arg isn't a table

--- config/nvim/lua/plongitudes/globals.lua
+++ config/nvim/lua/plongitudes/globals.lua
@@ -5,28 +5,35 @@ end
 
 --print a whole dang table
 function pp (tbl, indent)
-  if not indent then indent = 0 end
-  local toprint = string.rep(" ", indent) .. "{\r\n"
-  indent = indent + 2 
-  for k, v in pairs(tbl) do
-    toprint = toprint .. string.rep(" ", indent)
-    if (type(k) == "number") then
-      toprint = toprint .. "[" .. k .. "] = "
-    elseif (type(k) == "string") then
-      toprint = toprint  .. k ..  "= "   
+  --validate args
+  if (type(tbl) ~= "table") then
+    print("sorry, input isn't a table, it's a "..type(tbl)..".")
+    print(tbl)
+    return
+  else
+    if not indent then indent = 2 end
+    local toprint = string.rep(" ", indent) .. "{\r\n"
+    indent = indent + 2 
+    for k, v in pairs(tbl) do
+      toprint = toprint .. string.rep(" ", indent)
+      if (type(k) == "number") then
+        toprint = toprint .. "[" .. k .. "] = "
+      elseif (type(k) == "string") then
+        toprint = toprint  .. k ..  "= "
+      end
+      if (type(v) == "number") then
+        toprint = toprint .. v .. ",\r\n"
+      elseif (type(v) == "string") then
+        toprint = toprint .. "\"" .. v .. "\",\r\n"
+      elseif (type(v) == "table") then
+        toprint = toprint .. pp(v, indent + 2) .. ",\r\n"
+      else
+        toprint = toprint .. "\"" .. tostring(v) .. "\",\r\n"
+      end
     end
-    if (type(v) == "number") then
-      toprint = toprint .. v .. ",\r\n"
-    elseif (type(v) == "string") then
-      toprint = toprint .. "\"" .. v .. "\",\r\n"
-    elseif (type(v) == "table") then
-      toprint = toprint .. pp(v, indent + 2) .. ",\r\n"
-    else
-      toprint = toprint .. "\"" .. tostring(v) .. "\",\r\n"
-    end
+    toprint = toprint .. string.rep(" ", indent-2) .. "}"
+    return toprint
   end
-  toprint = toprint .. string.rep(" ", indent-2) .. "}"
-  return toprint
 end
 
 --[[
